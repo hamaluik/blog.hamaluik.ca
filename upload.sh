b141a0647208effddca7c6a197026d7f7b00b8ed
Kenton Hamaluik <kenton@hamaluik.com>
2017-04-22 16:16:53 -0600

    removed upload.sh in favour of git deployment

--- upload.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-read -p "Server: " server
-read -p "Username: " username
-read -p "Directory: " directory
-
-#rsync -r --info=progress2 public/* $username@$server:$directory
-rsync -r --progress public/* $username@$server:$directory
